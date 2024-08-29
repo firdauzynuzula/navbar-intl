53633b480ccbef3023f065d1623209b4133f6332
firdauzy <nuzulafirdhauzy@gmail.com>
2024-08-29 12:34:29 +0700

    update:content

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,9 @@
-import createMiddleware from "next-intl/middleware";
-
-export default createMiddleware({
-    // A list of all locales that are supported
-    locales: ["en", "id"],
-
-    // Used when no locale matches
-    defaultLocale: "en",
-});
-
+import createMiddleware from 'next-intl/middleware';
+import {routing} from './routing';
+ 
+export default createMiddleware(routing);
+ 
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ["/", "/(id|en)/:path*"],
+  // Match only internationalized pathnames
+  matcher: ['/','/(id|en)/:path*']
 };
